0639c91038d2d9e6b5fa346b762397bece83ce49
Mert Ozer <mertozer94@gmail.com>
2017-06-09 21:01:28 +0300

    Added video selection

---
+++ downloadVideo.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+# --download-archive for only new videos
+
+ydl_opts = {
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+    'ignoreerrors': True
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=CIvtiNpKEY0'])
